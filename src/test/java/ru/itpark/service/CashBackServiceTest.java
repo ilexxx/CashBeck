6fbcd2b91a951865a157617f2e08beef601d4599
student <student05@itpark.ru>
2018-12-02 16:17:49 +0300

    Covarige reguirements met

--- src/test/java/ru/itpark/service/CashBackServiceTest.java
+++ src/test/java/ru/itpark/service/CashBackServiceTest.java
@@ -1,5 +1,6 @@
 package ru.itpark.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashBackServiceTest {
 
     @Test
+    @DisplayName("should calculate cashback for")
     void cashback() {
+        CashBackService service = new CashBackService();
+        int result = service.cashback(1000);
+
+        assertEquals(50, result);
     }
+
 }
